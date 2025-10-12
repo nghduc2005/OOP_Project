59d30ba7e44438e715182bae69845fe26df55ad3
BVPZ <bvpz2005@gmail.com>
2025-10-13 03:38:57 +0700

    Update LoginPanelTeacher.java

--- Main/src/app/ui/LoginPanelTeacher.java
+++ Main/src/app/ui/LoginPanelTeacher.java
@@ -6,13 +6,15 @@ import app.service.TeacherService;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class LoginPanelTeacher extends JPanel {
-    JLabel usernameLabel, passwordLabel, titleLabel;
+    JLabel usernameLabel, passwordLabel, titleLabel,forgotPasswordLabel, registerLabel;
     JTextField username;
     JPasswordField password;
-    JButton loginButton;
+    JButton loginButton,ShowPassword;
     MainPanel mainPanel;
+    char defaultEchoChar;
     int W = Toolkit.getDefaultToolkit().getScreenSize().width;
     int H = Toolkit.getDefaultToolkit().getScreenSize().height;
 
@@ -25,20 +27,46 @@ public class LoginPanelTeacher extends JPanel {
         passwordLabel = new JLabel("Password");
         username = new JTextField();
         password = new JPasswordField();
-        loginButton = new JButton("Login");
-
+        loginButton = new JButton("Đăng nhập");
+        loginButton.setBackground(Color.BLUE);
+        loginButton.setForeground(Color.WHITE);
+        loginButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // chuyển thành bàn tay
+        forgotPasswordLabel = new JLabel("<HTML><U>Quên mật khẩu?</U></HTML>");
+        forgotPasswordLabel.setForeground(Color.BLUE);
+        forgotPasswordLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        registerLabel = new JLabel("<HTML><U>Đăng ký</U></HTML>");
+        registerLabel.setForeground(Color.GREEN);
+        registerLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        ShowPassword = new JButton("👁");
+        defaultEchoChar = password.getEchoChar();
         //Set các size
+        int y=10;
         titleLabel.setFont(new Font("Arial", Font.BOLD, 32));
-        titleLabel.setBounds(W/2-100,H/2-400,500,50);
-        usernameLabel.setBounds(W/2-200,H/2-300,100,30);
-        passwordLabel.setBounds(W/2-200,H/2-260,100,30);
-        username.setBounds(W/2-100,H/2-300,230,30);
-        password.setBounds(W/2-100,H/2-260,230,30);
-        loginButton.setBounds(W/2-100,H/2-200,230,30);
+        titleLabel.setBounds(W/2-100,H/2-200-y,500,50);
+        usernameLabel.setBounds(W/2-200,H/2-100-y,100,30);
+        passwordLabel.setBounds(W/2-200,H/2-60-y,100,30);
+        username.setBounds(W/2-100,H/2-100-y,230,30);
+        password.setBounds(W/2-100,H/2-60-y,230,30);
+        loginButton.setBounds(W/2-40,H/2-20-y,230-120,30);
+        ShowPassword.setBounds(W/2 - 100 +240 , H/2-55-y, 50, 20);
+        forgotPasswordLabel.setBounds(W/2-100+230-90,H/2+10-y,100,30);
+        registerLabel.setBounds(W/2-100,H/2+10-y,150,30);
 
         //Add các action
         loginButton.addActionListener(e->loginSubmit()); //dùng lambda rồi truyền logic theo từng component
-
+        forgotPasswordLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                updateLater();
+            }
+        });
+        registerLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                updateLater();
+            }
+        });
+        ShowPassword.addActionListener(e -> show_Password());
         //Add vào panel
         add(titleLabel);
         add(usernameLabel);
@@ -46,29 +74,13 @@ public class LoginPanelTeacher extends JPanel {
         add(username);
         add(password);
         add(loginButton);
-
-        //Set các thiết lập cho panel
-        JLabel forgotPasswordLabel = new JLabel("<HTML><U>Quên mật khẩu?</U></HTML>");
-        forgotPasswordLabel.setForeground(Color.BLUE);
-        forgotPasswordLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-
-        forgotPasswordLabel.setBounds(W/2-100+250,H/2-260,150,30);
-
-        // Thêm sự kiện click
-        forgotPasswordLabel.addMouseListener(new java.awt.event.MouseAdapter() {
-            @Override
-            public void mouseClicked(java.awt.event.MouseEvent e) {
-                JOptionPane.showMessageDialog(
-                        LoginPanelTeacher.this,
-                        "Tính năng khôi phục mật khẩu đang được phát triển!",
-                        "Quên mật khẩu",
-                        JOptionPane.INFORMATION_MESSAGE
-                );
-            }
-        });
-
-        // Thêm label vào panel
         add(forgotPasswordLabel);
+        add(ShowPassword);
+        add(registerLabel);
+        //Set các thiết lập cho panel
+
+
+
     }
     public void loginSubmit(){
         String username = this.username.getText();
@@ -82,4 +94,20 @@ public class LoginPanelTeacher extends JPanel {
         }
         mainPanel.show("dashboard");
     }
+    public void updateLater(){
+        JOptionPane.showMessageDialog(
+                LoginPanelTeacher.this,
+                "Tính năng này đang được phát triển!",
+                "Chú ý",
+                JOptionPane.INFORMATION_MESSAGE
+        );
+    }
+    public void show_Password(){
+        if (this.password.getEchoChar() == (char) 0) {
+            password.setEchoChar(defaultEchoChar); //ẩn
+        } else {
+            password.setEchoChar((char) 0);// hiển thị
+        }
+    }
+
 }
