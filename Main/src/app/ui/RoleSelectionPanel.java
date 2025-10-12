c533af96449056bd037c7ad4623ff84afb039213
BVPZ <bvpz2005@gmail.com>
2025-10-13 03:39:19 +0700

    Update RoleSelectionPanel.java

--- Main/src/app/ui/RoleSelectionPanel.java
+++ Main/src/app/ui/RoleSelectionPanel.java
@@ -6,6 +6,8 @@ import java.awt.*;
 public class RoleSelectionPanel extends JPanel {
     JButton teacherButton, studentButton;
     MainPanel mainPanel;
+    int W = Toolkit.getDefaultToolkit().getScreenSize().width;
+    int H = Toolkit.getDefaultToolkit().getScreenSize().height;
 
     public RoleSelectionPanel(MainPanel mainPanel) {
         this.mainPanel = mainPanel;
@@ -13,14 +15,14 @@ public class RoleSelectionPanel extends JPanel {
 
         JLabel title = new JLabel("You are : ");
         title.setFont(new Font("Arial", Font.BOLD, 18));
-        title.setBounds(620, 150, 300, 50);
+        title.setBounds(W/2-150, H/2-200, 300, 50);
 
         teacherButton = new JButton("Teacher");
         studentButton = new JButton("Student");
 
-        teacherButton.setBounds(650, 220, 200, 40);
-        studentButton.setBounds(650, 280, 200, 40);
-
+        teacherButton.setBounds(W/2-100, H/2-150, 200, 40);
+        studentButton.setBounds(W/2-100, H/2-150+60, 200, 40);
+        title.setForeground(Color.RED);
         // sự kiện bấm nút
         teacherButton.addActionListener(e -> mainPanel.show("Log_t"));
         studentButton.addActionListener(e -> mainPanel.show("Log_s"));
