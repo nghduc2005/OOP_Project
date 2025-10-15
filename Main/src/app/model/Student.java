336e8815268adea6a5a34f7682c28523769932a7
ki9z <KienTT.B23CN470@stu.ptit.edu.vn>
2025-10-15 20:04:13 +0700

    them getter setter

--- Main/src/app/model/Student.java
+++ Main/src/app/model/Student.java
@@ -11,4 +11,26 @@ public class Student extends User{
         super(request.getUsername(), request.getPassword());
         this.studentId = studentId;
     }
+
+
+    public Student(String studentId, String lastName, String firstName,
+                   String userName, String password, String phoneNumber,
+                   String email, LocalDate dateOfBirth) {
+        super(userName, password);
+        this.studentId = studentId;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
 }
