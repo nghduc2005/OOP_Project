ba6d9828262e652f720509d4cc89e9030c94b993
BVPZ <bvpz200@gmail.com>
2025-10-20 21:11:35 +0700

    Cập nhật mật khẩu

--- Main/src/app/dto/request/ChangePasswordRequest.java
+++ Main/src/app/dto/request/ChangePasswordRequest.java
@@ -1,5 +1,8 @@
 package app.dto.request;
 
+import app.model.Student;
+import app.session.Session;
+import app.dao.*;
 public class ChangePasswordRequest {
     private String oldPassword;
     private String newPassword;
@@ -21,5 +24,34 @@ public class ChangePasswordRequest {
     }
     public void setConfirmNewPassword(String confirmNewPassword) {
         this.confirmNewPassword = confirmNewPassword;
+    }
+    public static boolean updatePassword(String username, String newPassword) {
+        String role="";
+        if(Session.getRole().equals("Teacher")) role = "teachers";
+        else role= "students";
+        String query = String.format(
+                "UPDATE %s SET password = '%s' WHERE username = '%s'",
+                escapeString(role),
+                escapeString(newPassword),
+                escapeString(username)
+        );
+
+        try {
+            boolean result = DatabaseConnection.insertTable(query);
+            if (result) {
+                System.out.println("Đổi mật khẩu thành công ");
+            }
+            return result;
+        } catch (Exception e) {
+            System.out.println("Lỗi khi đổi mật khẩu: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+    private static String escapeString(String str) {
+        if (str == null) {
+            return "";
+        }
+        return str.replace("'", "''");
     }
 }
--- Main/src/app/ui/ChangePassword.java
+++ Main/src/app/ui/ChangePassword.java
@@ -1,5 +1,9 @@
 package app.ui;
 
+import app.dao.*;
+import app.dto.request.ChangePasswordRequest;
+import app.session.Session;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -32,12 +36,12 @@ public class ChangePassword extends JPanel {
         ChangePasswordLabel.setBounds(W/2-100,H/2-200-y,500,50);
 
         OldPasswordLabel.setBounds(W/2-230,H/2-100-y,100,30);
-        Re_enterPasswordLabel.setBounds(W/2-230,H/2-60-y,100,30);
-        NewPasswordLabel.setBounds(W/2-230,H/2-20-y,100,30);
+        NewPasswordLabel.setBounds(W/2-230,H/2-60-y,100,30);
+        Re_enterPasswordLabel.setBounds(W/2-230,H/2-20-y,100,30);
 
         OldPassword.setBounds(W/2-100,H/2-100-y,230,30);
-        Re_enterPassword.setBounds(W/2-100,H/2-60-y,230,30);
-        NewPassword.setBounds(W/2-100,H/2-20-y,230,30);
+        NewPassword.setBounds(W/2-100,H/2-60-y,230,30);
+        Re_enterPassword.setBounds(W/2-100,H/2-20-y,230,30);
 
         ContinueButton.setForeground(Color.WHITE);
         ContinueButton.setBackground(Color.BLUE);
@@ -59,8 +63,11 @@ public class ChangePassword extends JPanel {
         String pw =new String(this.OldPassword.getPassword());
         String check_pw = new String(this.Re_enterPassword.getPassword());
         String new_pw = new String(this.NewPassword.getPassword());
-        // mật khảu đúng trong db
-        String correct_pw = "abcd";
+        String user_name = Session.getUsername();
+        String correct_pw = "";
+        if(Session.getRole().equals("Teacher")) correct_pw = TeacherDao.getPasswordByUsername(user_name);
+        else correct_pw = StudentDao.getPasswordByUsername(user_name);
+        // Chưa nhập đầy đủ
         if (pw.isEmpty() || check_pw.isEmpty() || new_pw.isEmpty()){
             JOptionPane.showMessageDialog(
                     ChangePassword.this,
@@ -68,22 +75,34 @@ public class ChangePassword extends JPanel {
                     "Chú ý",
                     JOptionPane.WARNING_MESSAGE
             );
-        }
-        else if (!pw.equals(check_pw)){
+        }        // Mk nhập lại sai
+        else if (!new_pw.equals(check_pw)){
             JOptionPane.showMessageDialog(
                     ChangePassword.this,
                     "Mật khẩu nhập lại khác ",
                     "Chú ý",
                     JOptionPane.WARNING_MESSAGE
             );
-        } else if (pw.equals(correct_pw)) {
-            // đổi mk
-            JOptionPane.showMessageDialog(
-                    ChangePassword.this,
-                    "Đổi mật khẩu thành công ",
-                    "Thông báo",
-                    JOptionPane.INFORMATION_MESSAGE
-            );
+        }
+        else if (correct_pw.equals(pw)) {
+
+            if (ChangePasswordRequest.updatePassword(user_name,new_pw)){
+                JOptionPane.showMessageDialog(
+                        ChangePassword.this,
+                        "Đổi mật khẩu thành công ",
+                        "Thông báo",
+                        JOptionPane.INFORMATION_MESSAGE
+                );
+            }else{
+                JOptionPane.showMessageDialog(
+                        ChangePassword.this,
+                        "Đã có lỗi xảy ra, vui lòng thử lại ",
+                        "Thông báo",
+                        JOptionPane.WARNING_MESSAGE
+                );
+            }
+
+
         } else{
             JOptionPane.showMessageDialog(
                     ChangePassword.this,
@@ -93,5 +112,14 @@ public class ChangePassword extends JPanel {
             );
         }
     }
+//    public static void main(String[] args) {
+//        MainFrame mainFrame = new MainFrame();
+//        mainFrame.add(new ChangePassword(new MainPanel()));
+//        int W = Toolkit.getDefaultToolkit().getScreenSize().width;
+//        int H = Toolkit.getDefaultToolkit().getScreenSize().height;
+//
+//        mainFrame.setVisible(true);
+//    }
+
 
 }
