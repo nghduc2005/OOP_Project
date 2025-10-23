b1982d229be1e3b1e56907398d390c79180a2879
Trần Trung Kiên <KienTT.B23CN470@stu.ptit.edu.vn>
2025-10-23 10:16:58 +0700

    Update Teacher.java
    Thêm 1 constructor mới + getters/setters

--- Main/src/app/model/Teacher.java
+++ Main/src/app/model/Teacher.java
@@ -1,10 +1,32 @@
 package app.model;
 
 import app.dto.request.LoginRequest;
+import java.time.LocalDate;
 
 public class Teacher extends User{
     private String teacherId;
-    Teacher(LoginRequest request){
+    
+    public Teacher(LoginRequest request){
         super(request.getUsername(), request.getPassword());
+    }
+
+    public Teacher(String teacherId, String lastName, String firstName,
+                   String userName, String password, String phoneNumber,
+                   String email, LocalDate dateOfBirth) {
+        super(userName, password);
+        this.teacherId = teacherId;
+        setLastName(lastName);
+        setFirstName(firstName);
+        setPhoneNumber(phoneNumber);
+        setEmail(email);
+        setDateOfBirth(dateOfBirth);
+    }
+
+    public String getTeacherId() { 
+        return teacherId; 
+    }
+    
+    public void setTeacherId(String teacherId) { 
+        this.teacherId = teacherId; 
     }
 }
