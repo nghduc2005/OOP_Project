f10956630b0d4404d8f5178a34fedc269830ad81
Trần Trung Kiên <KienTT.B23CN470@stu.ptit.edu.vn>
2025-10-30 22:03:11 +0700

    Update Classes.java
    sua

--- Main/src/app/model/Classes.java
+++ Main/src/app/model/Classes.java
@@ -1,29 +1,87 @@
 package app.model;
-import java.util.*;
+
 public class Classes {
-    private String  class_id,teacher_id,total_student,subject_name;
-    private int maxnumberStudent;
-    public Classes(String class_id, String total_student, String subject_name,
-                   int m ){
-        this.maxnumberStudent = m;
-        this.class_id = class_id;
-        this.total_student = total_student;
-        this.subject_name=subject_name;
+    private Integer classId;
+    private Integer teacherId;
+    private Integer totalStudent;
+    private String subjectName;
+    private Integer maxNumberStudent;
+    private Integer subjectId;
+    private String scheduleId;
+    
+    public Classes() {}
+    
+    public Classes(Integer classId, Integer totalStudent, String subjectName, Integer maxNumberStudent) {
+        this.classId = classId;
+        this.totalStudent = totalStudent;
+        this.subjectName = subjectName;
+        this.maxNumberStudent = maxNumberStudent;
+    }
+    
+    public Classes(Integer classId, Integer teacherId, Integer totalStudent, 
+                   String subjectName, Integer maxNumberStudent, Integer subjectId, String scheduleId) {
+        this.classId = classId;
+        this.teacherId = teacherId;
+        this.totalStudent = totalStudent;
+        this.subjectName = subjectName;
+        this.maxNumberStudent = maxNumberStudent;
+        this.subjectId = subjectId;
+        this.scheduleId = scheduleId;
     }
 
-    public String getClass_id() {
-        return class_id;
+    public Integer getClassId() {
+        return classId;
     }
 
-    public int getMaxNumberStudent() {
-        return maxnumberStudent;
+    public void setClassId(Integer classId) {
+        this.classId = classId;
     }
 
-    public String getSubject_name() {
-        return subject_name;
+    public Integer getTeacherId() {
+        return teacherId;
     }
 
-    public String getTotal_student() {
-        return total_student;
+    public void setTeacherId(Integer teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public Integer getTotalStudent() {
+        return totalStudent;
+    }
+
+    public void setTotalStudent(Integer totalStudent) {
+        this.totalStudent = totalStudent;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public Integer getMaxNumberStudent() {
+        return maxNumberStudent;
+    }
+
+    public void setMaxNumberStudent(Integer maxNumberStudent) {
+        this.maxNumberStudent = maxNumberStudent;
+    }
+
+    public Integer getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(Integer subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(String scheduleId) {
+        this.scheduleId = scheduleId;
     }
 }
