f193508ffef28a924cd8dade1bc418856ac3dbfe
long_and_short <lnguyenhoang717@gmail.com>
2025-11-13 07:18:36 +0700

    Cập nhật Database mới

--- Main/src/app/Constant.java
+++ Main/src/app/Constant.java
@@ -12,9 +12,9 @@ public final class Constant {
     public final static String FIRSTNAME_PATTERN = "^[A-Za-zÀ-ỹ\\s]+$"; //cho phép latinh, tiếng việt, khoảng trắng
     public final static String PHONENUMBER_PATTERN = "^[0-9]{10}$";
 
-    public final static String DB_URL_CONNECT = "jdbc:mysql://caboose.proxy.rlwy.net:11411/railway";;
+    public final static String DB_URL_CONNECT = "jdbc:mysql://crossover.proxy.rlwy.net:35590/railway";
     public final static String DB_USER_CONNECT = "root";
-    public final static String DB_PASSWORD_CONNECT = "TVsizsRuzPhuzbVYUweDnKVpQUPQLxgp";
+    public final static String DB_PASSWORD_CONNECT = "wmhsqZmmgNFujhyCElFykkdkqigAzMTP";
 
     public final static String TEACHER_USERNAME = "NVA000001";
 }
